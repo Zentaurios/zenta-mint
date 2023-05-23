baf3b55c6f082662af74f16bfd847f13e2dc8ce6
ReisserCoder <reisscoding@gmail.com>
2023-05-23 10:27:40 -0500

    added Paper link

--- pages/drop.tsx
+++ pages/drop.tsx
@@ -1,231 +1,247 @@
 import {
-    MediaRenderer,
-    useActiveClaimConditionForWallet,
-    useAddress,
-    useClaimConditions,
-    useClaimedNFTSupply,
-    useClaimerProofs,
-    useClaimIneligibilityReasons,
-    useContract,
-    useContractMetadata,
-    useUnclaimedNFTSupply,
-    Web3Button,
-  } from "@thirdweb-dev/react";
-  import { BigNumber, utils } from "ethers";
-  import type { NextPage } from "next";
-  import Image from "next/image";
-  import { useMemo, useState } from "react";
-  import Timer from "../components/Timer";
-  import styles from "../styles/Theme.module.css";
-  import { parseIneligibility } from "../utils/parseIneligibility";
-  
-  // Put Your NFT Drop Contract address from the dashboard here
-  const myNftDropContractAddress = "0xa9c1D0646941fA510fd413F1749100053072e1D8";
-  
-  const Home: NextPage = () => {
-    const { contract: nftDrop } = useContract(myNftDropContractAddress);
-  
-    const address = useAddress();
-    const [quantity, setQuantity] = useState(1);
-  
-    const { data: contractMetadata } = useContractMetadata(nftDrop);
-  
-    const claimConditions = useClaimConditions(nftDrop);
-  
-    const activeClaimCondition = useActiveClaimConditionForWallet(
-      nftDrop,
-      address || ""
+  MediaRenderer,
+  useActiveClaimConditionForWallet,
+  useAddress,
+  useClaimConditions,
+  useClaimedNFTSupply,
+  useClaimerProofs,
+  useClaimIneligibilityReasons,
+  useContract,
+  useContractMetadata,
+  useUnclaimedNFTSupply,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { BigNumber, utils } from "ethers";
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
+import { useMemo, useState } from "react";
+import Timer from "../components/Timer";
+import styles from "../styles/Theme.module.css";
+import { parseIneligibility } from "../utils/parseIneligibility";
+
+// Put Your NFT Drop Contract address from the dashboard here
+const myNftDropContractAddress = "0xa9c1D0646941fA510fd413F1749100053072e1D8";
+
+const Home: NextPage = () => {
+  const { contract: nftDrop } = useContract(myNftDropContractAddress);
+
+  const address = useAddress();
+  const [quantity, setQuantity] = useState(1);
+
+  const { data: contractMetadata } = useContractMetadata(nftDrop);
+
+  const claimConditions = useClaimConditions(nftDrop);
+
+  const activeClaimCondition = useActiveClaimConditionForWallet(
+    nftDrop,
+    address || ""
+  );
+  const claimerProofs = useClaimerProofs(nftDrop, address || "");
+  const claimIneligibilityReasons = useClaimIneligibilityReasons(nftDrop, {
+    quantity,
+    walletAddress: address || "",
+  });
+  const unclaimedSupply = useUnclaimedNFTSupply(nftDrop);
+  const claimedSupply = useClaimedNFTSupply(nftDrop);
+
+  const numberClaimed = useMemo(() => {
+    return BigNumber.from(claimedSupply.data || 0).toString();
+  }, [claimedSupply]);
+
+  const numberTotal = useMemo(() => {
+    return BigNumber.from(claimedSupply.data || 0)
+      .add(BigNumber.from(unclaimedSupply.data || 0))
+      .toString();
+  }, [claimedSupply.data, unclaimedSupply.data]);
+
+  const priceToMint = useMemo(() => {
+    const bnPrice = BigNumber.from(
+      activeClaimCondition.data?.currencyMetadata.value || 0
     );
-    const claimerProofs = useClaimerProofs(nftDrop, address || "");
-    const claimIneligibilityReasons = useClaimIneligibilityReasons(nftDrop, {
-      quantity,
-      walletAddress: address || "",
-    });
-    const unclaimedSupply = useUnclaimedNFTSupply(nftDrop);
-    const claimedSupply = useClaimedNFTSupply(nftDrop);
-  
-    const numberClaimed = useMemo(() => {
-      return BigNumber.from(claimedSupply.data || 0).toString();
-    }, [claimedSupply]);
-  
-    const numberTotal = useMemo(() => {
-      return BigNumber.from(claimedSupply.data || 0)
-        .add(BigNumber.from(unclaimedSupply.data || 0))
-        .toString();
-    }, [claimedSupply.data, unclaimedSupply.data]);
-  
-    const priceToMint = useMemo(() => {
-      const bnPrice = BigNumber.from(
+    return `${utils.formatUnits(
+      bnPrice.mul(quantity).toString(),
+      activeClaimCondition.data?.currencyMetadata.decimals || 18
+    )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
+  }, [
+    activeClaimCondition.data?.currencyMetadata.decimals,
+    activeClaimCondition.data?.currencyMetadata.symbol,
+    activeClaimCondition.data?.currencyMetadata.value,
+    quantity,
+  ]);
+
+  const maxClaimable = useMemo(() => {
+    let bnMaxClaimable;
+    try {
+      bnMaxClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimableSupply || 0
+      );
+    } catch (e) {
+      bnMaxClaimable = BigNumber.from(1_000_000);
+    }
+
+    let perTransactionClaimable;
+    try {
+      perTransactionClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimablePerWallet || 0
+      );
+    } catch (e) {
+      perTransactionClaimable = BigNumber.from(1_000_000);
+    }
+
+    if (perTransactionClaimable.lte(bnMaxClaimable)) {
+      bnMaxClaimable = perTransactionClaimable;
+    }
+
+    const snapshotClaimable = claimerProofs.data?.maxClaimable;
+
+    if (snapshotClaimable) {
+      if (snapshotClaimable === "0") {
+        // allowed unlimited for the snapshot
+        bnMaxClaimable = BigNumber.from(1_000_000);
+      } else {
+        try {
+          bnMaxClaimable = BigNumber.from(snapshotClaimable);
+        } catch (e) {
+          // fall back to default case
+        }
+      }
+    }
+
+    const maxAvailable = BigNumber.from(unclaimedSupply.data || 0);
+
+    let max;
+    if (maxAvailable.lt(bnMaxClaimable)) {
+      max = maxAvailable;
+    } else {
+      max = bnMaxClaimable;
+    }
+
+    if (max.gte(1_000_000)) {
+      return 1_000_000;
+    }
+    return max.toNumber();
+  }, [
+    claimerProofs.data?.maxClaimable,
+    unclaimedSupply.data,
+    activeClaimCondition.data?.maxClaimableSupply,
+    activeClaimCondition.data?.maxClaimablePerWallet,
+  ]);
+
+  const isSoldOut = useMemo(() => {
+    try {
+      return (
+        (activeClaimCondition.isSuccess &&
+          BigNumber.from(activeClaimCondition.data?.availableSupply || 0).lte(
+            0
+          )) ||
+        numberClaimed === numberTotal
+      );
+    } catch (e) {
+      return false;
+    }
+  }, [
+    activeClaimCondition.data?.availableSupply,
+    activeClaimCondition.isSuccess,
+    numberClaimed,
+    numberTotal,
+  ]);
+
+  const canClaim = useMemo(() => {
+    return (
+      activeClaimCondition.isSuccess &&
+      claimIneligibilityReasons.isSuccess &&
+      claimIneligibilityReasons.data?.length === 0 &&
+      !isSoldOut
+    );
+  }, [
+    activeClaimCondition.isSuccess,
+    claimIneligibilityReasons.data?.length,
+    claimIneligibilityReasons.isSuccess,
+    isSoldOut,
+  ]);
+
+  const isLoading = useMemo(() => {
+    return (
+      activeClaimCondition.isLoading ||
+      unclaimedSupply.isLoading ||
+      claimedSupply.isLoading ||
+      !nftDrop
+    );
+  }, [
+    activeClaimCondition.isLoading,
+    nftDrop,
+    claimedSupply.isLoading,
+    unclaimedSupply.isLoading,
+  ]);
+
+  const buttonLoading = useMemo(
+    () => isLoading || claimIneligibilityReasons.isLoading,
+    [claimIneligibilityReasons.isLoading, isLoading]
+  );
+  const buttonText = useMemo(() => {
+    if (isSoldOut) {
+      return "Sold Out";
+    }
+
+    if (canClaim) {
+      const pricePerToken = BigNumber.from(
         activeClaimCondition.data?.currencyMetadata.value || 0
       );
-      return `${utils.formatUnits(
-        bnPrice.mul(quantity).toString(),
-        activeClaimCondition.data?.currencyMetadata.decimals || 18
-      )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
-    }, [
-      activeClaimCondition.data?.currencyMetadata.decimals,
-      activeClaimCondition.data?.currencyMetadata.symbol,
-      activeClaimCondition.data?.currencyMetadata.value,
-      quantity,
-    ]);
-  
-    const maxClaimable = useMemo(() => {
-      let bnMaxClaimable;
-      try {
-        bnMaxClaimable = BigNumber.from(
-          activeClaimCondition.data?.maxClaimableSupply || 0
-        );
-      } catch (e) {
-        bnMaxClaimable = BigNumber.from(1_000_000);
+      if (pricePerToken.eq(0)) {
+        return "Mint (Free)";
       }
-  
-      let perTransactionClaimable;
-      try {
-        perTransactionClaimable = BigNumber.from(
-          activeClaimCondition.data?.maxClaimablePerWallet || 0
-        );
-      } catch (e) {
-        perTransactionClaimable = BigNumber.from(1_000_000);
-      }
-  
-      if (perTransactionClaimable.lte(bnMaxClaimable)) {
-        bnMaxClaimable = perTransactionClaimable;
-      }
-  
-      const snapshotClaimable = claimerProofs.data?.maxClaimable;
-  
-      if (snapshotClaimable) {
-        if (snapshotClaimable === "0") {
-          // allowed unlimited for the snapshot
-          bnMaxClaimable = BigNumber.from(1_000_000);
-        } else {
-          try {
-            bnMaxClaimable = BigNumber.from(snapshotClaimable);
-          } catch (e) {
-            // fall back to default case
-          }
-        }
-      }
-  
-      const maxAvailable = BigNumber.from(unclaimedSupply.data || 0);
-  
-      let max;
-      if (maxAvailable.lt(bnMaxClaimable)) {
-        max = maxAvailable;
-      } else {
-        max = bnMaxClaimable;
-      }
-  
-      if (max.gte(1_000_000)) {
-        return 1_000_000;
-      }
-      return max.toNumber();
-    }, [
-      claimerProofs.data?.maxClaimable,
-      unclaimedSupply.data,
-      activeClaimCondition.data?.maxClaimableSupply,
-      activeClaimCondition.data?.maxClaimablePerWallet,
-    ]);
-  
-    const isSoldOut = useMemo(() => {
-      try {
-        return (
-          (activeClaimCondition.isSuccess &&
-            BigNumber.from(activeClaimCondition.data?.availableSupply || 0).lte(
-              0
-            )) ||
-          numberClaimed === numberTotal
-        );
-      } catch (e) {
-        return false;
-      }
-    }, [
-      activeClaimCondition.data?.availableSupply,
-      activeClaimCondition.isSuccess,
-      numberClaimed,
-      numberTotal,
-    ]);
-  
-    const canClaim = useMemo(() => {
-      return (
-        activeClaimCondition.isSuccess &&
-        claimIneligibilityReasons.isSuccess &&
-        claimIneligibilityReasons.data?.length === 0 &&
-        !isSoldOut
-      );
-    }, [
-      activeClaimCondition.isSuccess,
-      claimIneligibilityReasons.data?.length,
-      claimIneligibilityReasons.isSuccess,
-      isSoldOut,
-    ]);
-  
-    const isLoading = useMemo(() => {
-      return (
-        activeClaimCondition.isLoading ||
-        unclaimedSupply.isLoading ||
-        claimedSupply.isLoading ||
-        !nftDrop
-      );
-    }, [
-      activeClaimCondition.isLoading,
-      nftDrop,
-      claimedSupply.isLoading,
-      unclaimedSupply.isLoading,
-    ]);
-  
-    const buttonLoading = useMemo(
-      () => isLoading || claimIneligibilityReasons.isLoading,
-      [claimIneligibilityReasons.isLoading, isLoading]
-    );
-    const buttonText = useMemo(() => {
-      if (isSoldOut) {
-        return "Sold Out";
-      }
-  
-      if (canClaim) {
-        const pricePerToken = BigNumber.from(
-          activeClaimCondition.data?.currencyMetadata.value || 0
-        );
-        if (pricePerToken.eq(0)) {
-          return "Mint (Free)";
-        }
-        return `Mint (${priceToMint})`;
-      }
-      if (claimIneligibilityReasons.data?.length) {
-        return parseIneligibility(claimIneligibilityReasons.data, quantity);
-      }
-      if (buttonLoading) {
-        return "Checking eligibility...";
-      }
-  
-      return "Claiming not available";
-    }, [
-      isSoldOut,
-      canClaim,
-      claimIneligibilityReasons.data,
-      buttonLoading,
-      activeClaimCondition.data?.currencyMetadata.value,
-      priceToMint,
-      quantity,
-    ]);
-  
-    return (
+      return `Mint (${priceToMint})`;
+    }
+    if (claimIneligibilityReasons.data?.length) {
+      return parseIneligibility(claimIneligibilityReasons.data, quantity);
+    }
+    if (buttonLoading) {
+      return "Checking eligibility...";
+    }
+
+    return "Claiming not available";
+  }, [
+    isSoldOut,
+    canClaim,
+    claimIneligibilityReasons.data,
+    buttonLoading,
+    activeClaimCondition.data?.currencyMetadata.value,
+    priceToMint,
+    quantity,
+  ]);
+
+  return (
+    <>
+      <Head>
+        <title>Buy NFTs - Zentaurios Membership</title>
+        <meta name="description"
+          content="Buy NFTs with crypto, card, Google pay, or Apple Pay. Buy a Zentaurios Legacy NFT to support #Web3Community. NFTs as keys."
+        />
+        <meta property='og:title' content={"Buy NFTs - Zentaurios Membership"} />
+        <meta property='og:image' content={"/zentaurios-opensea-banner.png"} />
+        <meta property='og:description' content="Buy NFTs with crypto, card, Google pay, or Apple Pay. Buy a Zentaurios Legacy NFT to support #Web3Community. NFTs as keys." />
+        <meta property='og:url' content="https://legacy.zentaurios.app/drop" />
+        <meta property="fb:app_id" content="784086316061416" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <div className={styles.container}>
+        {!isLoading &&
+          <div className={styles.infoSide}>
+            {/* Title of your NFT Collection */}
+            <h1>{contractMetadata?.name}</h1>
+            {/* Description of your NFT Collection */}
+            <p className={styles.description}>
+              {contractMetadata?.description}
+            </p>
+          </div>
+        }
         <div className={styles.mintInfoContainer}>
           {isLoading ? (
             <p>Loading...</p>
           ) : (
             <>
-              <div className={styles.infoSide}>
-                {/* Title of your NFT Collection */}
-                <h1>{contractMetadata?.name}</h1>
-                {/* Description of your NFT Collection */}
-                <p className={styles.description}>
-                  {contractMetadata?.description}
-                </p>
-              </div>
-  
               <div className={styles.imageSide}>
                 {/* Image Preview of NFTs */}
                 <MediaRenderer
@@ -233,7 +249,7 @@ import {
                   src={contractMetadata?.image}
                   alt={`${contractMetadata?.name} preview image`}
                 />
-  
+
                 {/* Amount claimed so far */}
                 <div className={styles.mintCompletionArea}>
                   <div className={styles.mintAreaLeft}>
@@ -251,11 +267,11 @@ import {
                     )}
                   </div>
                 </div>
-  
+
                 {claimConditions.data?.length === 0 ||
-                claimConditions.data?.every(
-                  (cc) => cc.maxClaimableSupply === "0"
-                ) ? (
+                  claimConditions.data?.every(
+                    (cc) => cc.maxClaimableSupply === "0"
+                  ) ? (
                   <div>
                     <h2>
                       This drop is not ready to be minted yet. (No claim condition
@@ -278,9 +294,9 @@ import {
                       >
                         -
                       </button>
-  
+
                       <h4>{quantity}</h4>
-  
+
                       <button
                         className={`${styles.quantityControlButton}`}
                         onClick={() => setQuantity(quantity + 1)}
@@ -289,7 +305,7 @@ import {
                         +
                       </button>
                     </div>
-  
+
                     <div className={styles.mintContainer}>
                       {isSoldOut ? (
                         <div>
@@ -319,16 +335,18 @@ import {
             </>
           )}
         </div>
-        <div style={{ maxHeight: "200px", maxWidth: "100vw", overflow: "hidden" }}>
-            <Image
-              alt="Zentaurios Legacy NFT Banner on OpenSea #Zentaurios"
-              src="/zentaurios-opensea-banner.png"
-              width={800}
-              height={200}
-            />
-          </div>
+        <div style={{ maxHeight: "200px", maxWidth: "100vw", overflow: "hidden", textAlign: "center", marginTop: "-60px", marginBottom: "20px" }}>
+          <p>Or pay with card, Google Pay, or ApplePay on <Link href="https://withpaper.com/checkout/1b18e9d0-b760-4ee2-a63a-465a6fc100df" target="_blank" >Paper</Link>.</p>
+          <Image
+            alt="Zentaurios Legacy NFT Banner on OpenSea #Zentaurios"
+            src="/zentaurios-opensea-banner.png"
+            width={800}
+            height={200}
+          />
+        </div>
       </div>
-    );
-  };
-  
-  export default Home;
+    </>
+  );
+};
+
+export default Home;
